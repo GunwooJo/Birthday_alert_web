e7e6d54d802d00f34bfcba388365c4c3496541ba
GunwooJo <kanggi1997@naver.com>
2024-01-29 19:29:50 +0900

    feat: 친구추가 서비스코드 작성

---
+++ backend/src/main/java/gunwoo/birthday_alert/service/FriendService.java
@@ -0,0 +1,21 @@
+package gunwoo.birthday_alert.service;
+
+import gunwoo.birthday_alert.entity.Friend;
+import gunwoo.birthday_alert.repository.FriendRepository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class FriendService {
+
+    private final FriendRepository friendRepository;
+
+    @Transactional(readOnly = false)
+    public Long join(Friend friend) {
+        return friendRepository.save(friend);
+    }
+}
