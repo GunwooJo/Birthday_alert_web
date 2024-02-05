05fa0ca6d91c0606eeef229499bf1f3067ffda72
GunwooJo <kanggi1997@naver.com>
2024-02-05 22:50:53 +0900

    feat: 친구목록 페이지

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import UserRegister from "./pages/UserRegister";
 import AddFriend from "./pages/AddFriend";
+import FriendList from "./pages/FriendList";
 import {Route, Routes} from "react-router-dom";
 function App() {
     return (
         <Routes>
             <Route path='/friend/addBirthday' element={<AddFriend/>} />
+            <Route path='/friend/list' element={<FriendList/>} />
             <Route path='/user/register' element={<UserRegister/>} />
         </Routes>
     );
--- frontend/src/pages/FriendList.tsx
+++ frontend/src/pages/FriendList.tsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
+import ListGroup from 'react-bootstrap/ListGroup';
+
+interface FriendBirthday {
+    name: string;
+    birthday: string;
+}
 
 function FriendList() {
+    //임시 데이터
+    const [friends, setFriends] = useState<FriendBirthday[]>([
+        {name: '김철수', birthday: '2021-01-01'},
+        {name: '박영희', birthday: '2021-02-01'},
+        {name: '이철수', birthday: '2021-03-01'},
+    ]);
+
+    const handleFriendClick = (event: React.MouseEvent) => {
+        const index = (event.target as HTMLElement).dataset.index;
+        if (index !== undefined) {
+            alert(`친구 상세정보 페이지로 이동: ${friends[Number(index)].name}`);
+        }
+    }
+
     return (
         <div>
             <h1>친구목록</h1>
-
+            <ListGroup>
+                {friends.map((friend, index) => (
+                    <ListGroup.Item action key={index} data-index={index} onClick={handleFriendClick}>
+                        {friend.name} {friend.birthday}
+                    </ListGroup.Item>
+                ))}
+            </ListGroup>
         </div>
     );
 }
+
+export default FriendList;
