9c227b68f2850d28d6d210233c1c728a48d2e7e1
GunwooJo <kanggi1997@naver.com>
2024-02-13 13:56:52 +0900

    feat: 친구 찾는 코드(FriendRepository)

--- backend/src/main/java/gunwoo/birthday_alert/repository/FriendRepository.java
+++ backend/src/main/java/gunwoo/birthday_alert/repository/FriendRepository.java
@@ -5,6 +5,8 @@ import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class FriendRepository {
@@ -14,6 +16,12 @@ public class FriendRepository {
     public Long save(Friend friend) {
         em.persist((friend));
         return friend.getId();
+    }
+
+    public List<Friend> findByMemberId(Long memberId) {
+        return em.createQuery("select f from Friend f where f.member = :memberId", Friend.class)
+                .setParameter("memberId", memberId)
+                .getResultList();
     }
 
 }
