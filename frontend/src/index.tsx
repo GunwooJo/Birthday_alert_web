dda0f7a83a923cdfeee966e0f9323e29347c02c8
GunwooJo <kanggi1997@naver.com>
2024-02-14 17:46:05 +0900

    react-query 설치 후 셋팅

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,16 +5,21 @@ import App from "./App";
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 const rootElement = document.getElementById('root');
 if(!rootElement) throw new Error('rootElement 찾는데 실패함.');
 const root = createRoot(rootElement);
+const queryClient = new QueryClient();
+
 root.render(
-  <React.StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </QueryClientProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
