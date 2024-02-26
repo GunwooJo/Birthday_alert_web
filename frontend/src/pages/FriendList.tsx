7b17511f75016332995cac5c225ae8b9e2aee0be
GunwooJo <kanggi1997@naver.com>
2024-02-26 15:04:16 +0900

    react-query 사용 시도(타입 제대로 지정해야함)

--- frontend/src/pages/FriendList.tsx
+++ frontend/src/pages/FriendList.tsx
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 import ListGroup from 'react-bootstrap/ListGroup';
+import axios from "axios";
+import {useQuery} from "@tanstack/react-query";
 
 interface FriendBirthday {
     name: string;
@@ -20,12 +22,31 @@ function FriendList() {
             alert(`친구 상세정보 페이지로 이동: ${friends[Number(index)].name}`);
         }
     }
+    /* react-query 사용법
+        https://musma.github.io/2023/09/14/react-query.html
+    */
+    const getFriendList = async (email: string | null): Promise<FriendBirthday[]> => {
+        return fetch(`/member/showFriends?email=${email}`).then((response) => response.json());
+    }
+    const myEmail: string | null = localStorage.getItem("userEmail");
+    const {data, isError, error, isLoading} = useQuery<FriendBirthday[], Error>({
+        queryKey: ['friends'],
+        queryFn: () => getFriendList(myEmail)
+    });
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (isError) {
+        return <div>Error: {error.message}</div>;
+    }
 
     return (
         <div>
             <h1>친구목록</h1>
             <ListGroup>
-                {friends.map((friend, index) => (
+                {data.map((friend, index) => (
                     <ListGroup.Item action key={index} data-index={index} onClick={handleFriendClick}>
                         {friend.name} {friend.birthday}
                     </ListGroup.Item>
