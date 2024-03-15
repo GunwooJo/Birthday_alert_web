96c6fc08be7354ce4289e2f7e5728b4e05f80bef
GunwooJo <kanggi1997@naver.com>
2024-03-15 16:02:56 +0900

    친구 상세페이지 디자인

--- frontend/src/pages/FriendDetail.jsx
+++ frontend/src/pages/FriendDetail.jsx
@@ -2,11 +2,23 @@
 import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import axios from "axios";
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
 
 function FriendDetail() {
 
     const {friendId} = useParams();
     const [friend, setFriend] = useState({});
+    const [memberGetGift, setMemberGetGift] = useState([
+        'Cras justo odio',
+        'Cras justo odio',
+        'Cras justo odio'
+    ]);
+    const [friendGetGift, setFriendGetGift] = useState([
+        'Cras justo odio',
+        'Cras justo odio',
+        'Cras justo odio'
+    ]);
 
     useEffect(() => {
         fetchFriendDetail();
@@ -28,10 +40,50 @@ function FriendDetail() {
 
     return (
         <div>
-            <h1>친구 상세페이지</h1>
-            <div>{friendId}</div>
-            <div>이름: {friend.name}</div>
-            <div>생일: {friend.birthday}</div>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '20vh'
+            }}>
+                <h3>{friend.name}님과의 추억</h3>
+                <h4>생일: {friend.birthday}</h4>
+            </div>
+
+            <div style={{
+                display: 'flex',
+                justifyContent: 'space-around'
+            }}>
+                <div>
+                    <h4 style={{textAlign: 'center'}}>받은 선물 목록</h4>
+                    <ListGroup>
+                        {memberGetGift.map((gift, idx)=>{
+                            return (
+                                <ListGroup.Item key={gift + idx}>{gift}</ListGroup.Item>
+                            )
+                        })}
+                    </ListGroup>
+                    <div style={{ display: 'flex', justifyContent: 'center' }}>
+                        <Button variant="success">선물추가</Button>
+                    </div>
+                </div>
+
+                <div>
+                    <h4 style={{textAlign: 'center'}}>준 선물 목록</h4>
+                    <ListGroup>
+                        {friendGetGift.map((gift, idx) => {
+                            return (
+                                <ListGroup.Item key={gift + idx}>{gift}</ListGroup.Item>
+                            )
+                        })}
+                    </ListGroup>
+                    <div style={{display: 'flex', justifyContent: 'center'}}>
+                        <Button variant="success">선물추가</Button>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 }
